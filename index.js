f384ab117c357030571b0159f41714b377b527f5
Tanush Yadav <tanushyadav@gmail.com>
2021-03-16 13:00:22 +0530

    setting up express/heroku

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
